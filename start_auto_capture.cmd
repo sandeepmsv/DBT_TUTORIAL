0d3c0afcab3bf473aaf4ba80e645506572e7507f
sandeep <sandeepmsv001@gmail.com>
2026-07-06 17:54:58 +0530

    updated

--- start_auto_capture.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-setlocal
-cd /d "c:\Users\sande\OneDrive\Documents\DBT_TUTORIAL"
-powershell -NoProfile -ExecutionPolicy Bypass -File "c:\Users\sande\OneDrive\Documents\DBT_TUTORIAL\CaptureTerminalSummary.ps1"
